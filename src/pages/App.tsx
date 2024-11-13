006769609e4cf4179c44fe60f0e42f00a6c97407
Leuta Taras <81424893+tar1kkk@users.noreply.github.com>
2024-11-13 13:04:31 +0200

    init project

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+  return (
+    <div>
+      <Button variant='outline' >Нажми</Button>
+    </div>
+  )
+}
+
+export default App
